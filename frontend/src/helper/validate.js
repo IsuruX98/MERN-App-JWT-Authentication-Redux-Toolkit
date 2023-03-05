8ae66e26b51dad8f4dc033ab002019ef03d86c4a
IT21169380 <isurusanka98@gmail.com>
2023-03-05 13:00:20 +0530

    formik validations added

---
+++ frontend/src/helper/validate.js
@@ -0,0 +1,18 @@
+import toast from "react-hot-toast";
+
+//validate login page username
+export async function usernameValidate(values) {
+  const errors = usernameVerify({}, values);
+
+  return errors;
+}
+
+//validate username
+function usernameVerify(error = {}, values) {
+  if (!values.username) {
+    error.username = toast.error("username required...");
+  } else if (values.username.includes(" ")) {
+    error.username = toast.error("Inavalid Username...");
+  }
+  return error;
+}

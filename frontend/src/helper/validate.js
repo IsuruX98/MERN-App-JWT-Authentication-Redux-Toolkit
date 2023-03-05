8a6211bdf64d566c9c1ad23c068d9b6da7812a83
IT21169380 <isurusanka98@gmail.com>
2023-03-05 14:01:37 +0530

    reset password completed

--- frontend/src/helper/validate.js
+++ frontend/src/helper/validate.js
@@ -7,12 +7,52 @@ export async function usernameValidate(values) {
   return errors;
 }
 
-//validate username
+//validate login password
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+}
+
+//validate Reset password
+export async function resetPasswordValidate(values) {
+  const errors = passwordVerify({}, values);
+
+  if (values.password !== values.confirm_pwd) {
+    errors.exist = toast.error("Password missmatch...!");
+  }
+
+  return errors;
+}
+
+/*********************************************************/
+
+//Verify reset password
+
+//Verify password
+function passwordVerify(error = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    error.password = toast.error("password required...");
+  } else if (values.password.includes(" ")) {
+    error.password = toast.error("Wrong Password...!");
+  } else if (values.password.length < 4) {
+    error.password = toast.error(
+      "Password must be more than four Characters long..."
+    );
+  } else if (!specialChars.test(values.password)) {
+    error.password = toast.error("Password must have special Character ...");
+  }
+  return error;
+}
+
+//Verify username
 function usernameVerify(error = {}, values) {
   if (!values.username) {
     error.username = toast.error("username required...");
   } else if (values.username.includes(" ")) {
-    error.username = toast.error("Inavalid Username...");
+    error.username = toast.error("Wrong Username...!");
   }
   return error;
 }

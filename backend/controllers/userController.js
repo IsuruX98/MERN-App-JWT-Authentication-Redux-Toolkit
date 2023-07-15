3f56608e422c33a92d246d95cf97421f6731bb56
isuruX98 <isurusanka98@gmail.com>
2023-07-15 11:20:04 +0530

    first route cheaked

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 //@desc     Auth user/set token
 //route     POST /api/users/auth
 //@access   Public
-const authUser = (req, res) => {
+const authUser = async(req, res) => {
   res.status(200).json({ message: "Auth user" });
 };
 

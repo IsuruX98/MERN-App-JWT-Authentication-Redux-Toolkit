f618f0e3fe138b1c049a4eed105bed90853a28a0
isuruX98 <isurusanka98@gmail.com>
2023-07-15 09:43:25 +0530

    first route created

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+app.use("api/user", userRoutes);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+// - **POST /api/users** - Register a user
+// - **POST /api/users/auth** - authenticate a user and get token
+// - **POST /api/users/logout** - logout user and clear cookie
+// - **GET /api/users/profile** - get user profile
+// - **PUT /api/users/profile** - update profile
+
+app.listen(port, () => {
+  console.log(`Server stared on port ${port} 🚀`);
+});
